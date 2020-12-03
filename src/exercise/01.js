2a9103ac11adaa0f084286cbb92ee032715e5215
Davy Hausser <davy.hausser@gmail.com>
2020-12-03 09:22:37 +0100

    Code splitting extra credit  complete: 2. 💯 webpack magic comments

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,8 +3,7 @@
 
 import * as React from 'react'
 
-const loadGlobe = () => import(/* webpackPrefetch: true */ '../globe')
-const Globe = React.lazy(loadGlobe)
+const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -20,23 +19,19 @@ function App() {
         padding: '2rem',
       }}
     >
-      <React.Suspense fallback={<div>loading globe...</div>}>
-        <label
-          style={{marginBottom: '1rem'}}
-          onMouseEnter={loadGlobe}
-          onFocus={loadGlobe}
-        >
-          <input
-            type="checkbox"
-            checked={showGlobe}
-            onChange={e => setShowGlobe(e.target.checked)}
-          />
-          {' show globe'}
-        </label>
-        <div style={{width: 400, height: 400}}>
+      <label style={{marginBottom: '1rem'}}>
+        <input
+          type="checkbox"
+          checked={showGlobe}
+          onChange={e => setShowGlobe(e.target.checked)}
+        />
+        {' show globe'}
+      </label>
+      <div style={{width: 400, height: 400}}>
+        <React.Suspense fallback={<div>loading globe...</div>}>
           {showGlobe ? <Globe /> : null}
-        </div>
-      </React.Suspense>
+        </React.Suspense>
+      </div>
     </div>
   )
 }
